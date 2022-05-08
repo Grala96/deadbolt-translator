bc504b564f1f1cbf7283b499dd95834a0be89cea
Grala96 <gracjan.grala.tel@gmail.com>
2022-05-08 17:36:01 +0200

    Added checksum sha256 method for utils

---
+++ translator/src/main/java/utils/CheckSum.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
+public abstract class CheckSum {
+
+    public static String sha256(final ArrayList<Character> base) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.toString().getBytes(StandardCharsets.UTF_8));
+            final StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                final String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
